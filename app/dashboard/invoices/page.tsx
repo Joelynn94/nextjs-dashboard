71a183ec24004e4714ade57ca8a69651a9cadd6b
Joseph Lynn <josephlynn94@gmail.com>
2023-12-02 11:18:42 -0500

    style: reformat using 2 spaces

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -5,7 +5,7 @@ import { CreateInvoice } from '@/app/ui/invoices/buttons';
 import { lusitana } from '@/app/ui/fonts';
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 import { Suspense } from 'react';
- 
+
 export default async function Page() {
   return (
     <div className="w-full">
